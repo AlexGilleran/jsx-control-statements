5afd31c97e35da7f548a733f56d52b9fb19462f3
texttechne <gedankengut@gmx.de>
2016-01-04 13:44:10 +0100

    Improvement: use JSXExpressionContainer or JSXText as child (babel only)

---
+++ spec/fixtures/for-with-expression-container.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function () {
+    this.test = 'test';
+
+    return (
+      <div>
+        <For each="item" of={this.props.items}>
+          {item}
+        </For>
+      </div>
+    );
+  }
+});
---
+++ spec/fixtures/if-with-expression-container.jsx
@@ -0,0 +1,15 @@
+﻿var React = require('react');
+
+module.exports = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <If condition={this.props.condition}>
+          {'if rendered'}
+        <Else />
+          {'else rendered'}
+        </If>
+      </div>
+    );
+  }
+});
