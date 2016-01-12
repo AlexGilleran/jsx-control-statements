0b350d1ecc19c2bdce923dc268db157f242dcf26
texttechne <gedankengut@gmx.de>
2016-01-12 01:47:08 +0100

    Refactoring <For> statement

--- src/forStatement.js
+++ src/forStatement.js
@@ -2,53 +2,55 @@ var _ = require('lodash');
 var error = require('./error');
 
 
+function getAttributeMap(node) {
+  if (!node.openingElement || !node.openingElement.attributes) {
+    return {};
+  }
+
+  return _.reduce(node.openingElement.attributes, function(result, attr) {
+    if (attr.name && attr.name.name) {
+      result[attr.name.name] = attr;
+    }
+    return result;
+  }, {});
+}
+
+function addMapParam(types, params, attribute) {
+  if (attribute && attribute.value) {
+    params.push(types.Identifier(attribute.value.value))
+  }
+}
+
+
 module.exports = function(babel) {
   var types = babel.types;
 
   return function(node, file) {
-    var children, child, mapParams = [];
-    var attributes = node.openingElement.attributes;
+    var child, mapParams = [];
+    var attributes = getAttributeMap(node);
+    var children = types.react.buildChildren(node);
 
-    if (!attributes || !attributes.length) {
+    // required attribute
+    if (!attributes.of) {
       error.throwError(error.FOR_WITH_NO_ATTRIBUTES, node, file);
     }
-
-    var each, of, index;
-    attributes.forEach(function (attr) {
-      if (attr.name.name === 'each') {
-        each = attr;
-      }
-      else if (attr.name.name === 'of') {
-        of = attr;
-      }
-      else if (attr.name.name === 'index') {
-        index = attr;
-      }
-    });
-
-    if (!of) {
-      error.throwError(error.FOR_WITH_NO_ATTRIBUTES, node, file);
-    }
-
-    children = types.react.buildChildren(node);
+    //if (!attributes.of.value)
     if (children.length > 1) {
       error.throwError(error.MULTIPLE_CHILDREN, node, file);
     }
-    if (children.length === 0) {
+
+    // simply return without any child nodes
+    if (!children.length) {
       return types.NullLiteral();
     }
-    child = children[0];
 
-    if (each) {
-      mapParams.push(types.Identifier(each.value.value));
-    }
-    if (index) {
-      mapParams.push(types.Identifier(index.value.value));
-    }
+    child = children[0];
+    addMapParam(types, mapParams, attributes.each);
+    addMapParam(types, mapParams, attributes.index);
 
     return types.callExpression(
       types.memberExpression(
-        of.value.expression,
+        attributes.of.value.expression,
         types.identifier('map')
       ),
       [
