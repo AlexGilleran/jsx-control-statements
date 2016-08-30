edba2184e33c7b615faef1cf6edf32159e4616e1
Alex Gilleran <alex@alexgilleran.com>
2016-08-30 20:59:28 +1000

    Added flow types.

---
+++ jsx-control-statements.flow.js
@@ -0,0 +1,7 @@
+// @flow
+
+declare var If: ReactClass<{condition: boolean}>;
+declare var For: ReactClass<{each: string, index: string, of: Array<any>}>;
+declare var Choose: ReactClass<{}>;
+declare var When: ReactClass<{condition: boolean}>;
+declare var Otherwise: ReactClass<{}>;
