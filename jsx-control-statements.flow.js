acf380cdc315d858ff5da99404f1dc7172e4f722
Alex Gilleran <alex@alexgilleran.com>
2017-08-20 14:04:15 +1000

    Added defs for Flow 0.53 (thanks mwiencek!)

--- jsx-control-statements.flow.js
+++ jsx-control-statements.flow.js
@@ -1,7 +1,11 @@
 // @flow
 
-declare var If: ReactClass<{condition: boolean}>;
-declare var For: ReactClass<{each: string, index: string, of: Array<any>}>;
+// DEPRECATION NOTICE: If you're using Flow >= 0.53 please use the defs in the
+// jsx-control-statements.latest.flow.js file. These are deprecated and will
+// eventually be replaced by the defs in that file.
+
+declare var If: ReactClass<{ condition: boolean }>;
+declare var For: ReactClass<{ each: string, index: string, of: Array<any> }>;
 declare var Choose: ReactClass<{}>;
-declare var When: ReactClass<{condition: boolean}>;
+declare var When: ReactClass<{ condition: boolean }>;
 declare var Otherwise: ReactClass<{}>;
---
+++ jsx-control-statements.latest.flow.js
@@ -0,0 +1,7 @@
+// @flow
+
+declare var If: React$ComponentType<{condition: boolean}>;
+declare var For: React$ComponentType<{each: string, index: string, of: Array<any>}>;
+declare var Choose: React$ComponentType<{}>;
+declare var When: React$ComponentType<{condition: boolean}>;
+declare var Otherwise: React$ComponentType<{}>;
